71726c9228678f9f825714e4399ec38b22da5367
Jessica Vargas <139159958+javar10@users.noreply.github.com>
2024-06-18 22:13:15 -0400

    useState

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -4,6 +4,7 @@ export const selectAllCampsites = () => {
     return CAMPSITES;
 };
 
-export const selectRandomCampsite = () => {
-    return CAMPSITES[Math.floor(Math.random() * CAMPSITES.length)];
-};
+export const selectCampsiteById = (id) => {
+    return CAMPSITES.find((campsite) => campsite.id === id)
+}
+
